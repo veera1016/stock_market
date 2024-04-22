71872c6ecf27bdb53427a00602264d80a45ccb5c
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-04-22 19:38:26 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+# Alpha Vantage API URL and API key
+API_KEY = 'YOUR_API_KEY'
+API_URL = 'https://www.alphavantage.co/query'
+
+@app.route('/')
+def homepage():
+    symbol = 'AAPL'  # Example stock symbol
+    price = fetch_stock_price(symbol)
+    return render_template('homepage.html', symbol=symbol, price=price)
+
+def fetch_stock_price(symbol):
+    params = {
+        'function': 'GLOBAL_QUOTE',
+        'symbol': symbol,
+        'apikey': API_KEY
+    }
+    response = requests.get(API_URL, params=params)
+    data = response.json()
+    if 'Global Quote' in data:
+        return data['Global Quote']['05. price']
+    return None
+
+if __name__ == '__main__':
+    app.run(debug=True)
